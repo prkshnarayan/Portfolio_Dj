4f4198ab80643abc3add7193a8ed86be3a68fd29
prkshnarayan <jayaprakashbommisetty2580@gmail.com>
2025-04-19 13:49:17 +0530

    first commit1

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$@*m2j$d!b^v%l8+p-5g3w6%s7=@eu!kozn^dzv(f89-$j-&-w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- app1/urls.py
+++ app1/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('submit_contact/', views.submit_contact, name='submit_contact'),
+    path('view_submissions/', views.view_submissions)
     # your other URLs...
 ]
